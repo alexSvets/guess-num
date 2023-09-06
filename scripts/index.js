ca9e29cf6fe4692ca28169f9e5911caad830f847
Sasha <sasha.shvets.2017@mail.ru>
2023-09-06 18:17:49 +0300

    end work popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,26 @@
-// Event button play
 const play = document.querySelector(".game__play");
 const container = document.querySelector(".game__list");
 const buttons = document.querySelectorAll(".game__button");
+const counter = document.querySelector(".game__tries");
+const text = document.querySelector(".game__counter")
 const secretNum = Math.ceil(Math.random() * 10);
 const popup = document.querySelector(".popup");
+const popupWon = document.querySelector(".popup_type-won");
+const popupLose = document.querySelector(".popup_type-lose");
+const popupButtonWon = document.querySelector(".popup__button-won");
+const popupButtonLose = document.querySelector(".popup__button-lose");
 let tries = 0;
 
-console.log(secretNum);
 function guessNum(num) {
-  console.log(tries, "Попытки");
-  if (tries >= 5) {
-    alert("У вас закончились попытки");
+  if (tries >= 4) {
+    popupLose.classList.add("popup_is-openned");
   }
   if (secretNum == num) {
-    alert("Вы победили");
+    popupWon.classList.add("popup_is-openned");
   }
   if (secretNum != num) {
     tries++;
-    alert(`Вы не угадали. Осталось ${tries} попыток`);
+    counter.textContent = tries;
   }
 }
 
@@ -30,6 +33,14 @@ buttons.forEach((button) => {
 function hangingButtons() {
   container.style.display = "flex";
   play.style.display = "none";
+  text.style.display = "block"
+}
+
+function popupClose(popup) {
+  popup.classList.remove("popup_is-openned");
+  window.location.reload();
 }
 
 play.addEventListener("click", hangingButtons);
+popupButtonWon.addEventListener("click", () => popupClose(popupWon));
+popupButtonLose.addEventListener("click", () => popupClose(popupLose));
